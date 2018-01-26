13e3c266db54215fe2b9415b1a05a139c851169e
decapo <edahbashi@busbycompanies.com>
2018-01-25 22:06:02 -0600

    updated the CSRF form stuff

--- target/scala-2.12/twirl/main/views/html/createForm.template.scala
+++ target/scala-2.12/twirl/main/views/html/createForm.template.scala
@@ -15,16 +15,16 @@ import play.api.templates.PlayMagic._
 import play.api.mvc._
 import play.api.data._
 
-object createForm extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template3[Form[Computer],Seq[scala.Tuple2[String, String]],Messages,play.twirl.api.HtmlFormat.Appendable] {
+object createForm extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template4[Form[Computer],Seq[scala.Tuple2[String, String]],RequestHeader,Messages,play.twirl.api.HtmlFormat.Appendable] {
 
   /**/
-  def apply/*1.2*/(computerForm: Form[Computer], companies: Seq[(String, String)])(implicit messages: Messages):play.twirl.api.HtmlFormat.Appendable = {
+  def apply/*1.2*/(computerForm: Form[Computer], companies: Seq[(String, String)])(implicit request: RequestHeader, messages: Messages):play.twirl.api.HtmlFormat.Appendable = {
     _display_ {
       {
 /*3.2*/import helper._
 
 implicit def /*5.2*/implicitFieldConstructor/*5.26*/ = {{ FieldConstructor(twitterBootstrapInput.f) }};
-Seq[Any](format.raw/*1.95*/("""
+Seq[Any](format.raw/*1.119*/("""
 
 """),format.raw/*4.1*/("""
 """),format.raw/*5.75*/(""" 
@@ -34,9 +34,9 @@ Seq[Any](format.raw/*1.95*/("""
     """),format.raw/*9.5*/("""<h1>Add a computer</h1>
     
     """),_display_(/*11.6*/form(routes.Application.save())/*11.37*/ {_display_(Seq[Any](format.raw/*11.39*/("""
-        
+       	"""),_display_(/*12.10*/helper/*12.16*/.CSRF.formField),format.raw/*12.31*/(""" 
         """),format.raw/*13.9*/("""<fieldset>
-        
+       
             """),_display_(/*15.14*/inputText(computerForm("name"), '_label -> "Computer name")),format.raw/*15.73*/("""
             """),_display_(/*16.14*/inputText(computerForm("introduced"), '_label -> "Introduced date")),format.raw/*16.81*/("""
             """),_display_(/*17.14*/inputText(computerForm("discontinued"), '_label -> "Discontinued date")),format.raw/*17.85*/("""
@@ -64,9 +64,9 @@ Seq[Any](format.raw/*1.95*/("""
     }
   }
 
-  def render(computerForm:Form[Computer],companies:Seq[scala.Tuple2[String, String]],messages:Messages): play.twirl.api.HtmlFormat.Appendable = apply(computerForm,companies)(messages)
+  def render(computerForm:Form[Computer],companies:Seq[scala.Tuple2[String, String]],request:RequestHeader,messages:Messages): play.twirl.api.HtmlFormat.Appendable = apply(computerForm,companies)(request,messages)
 
-  def f:((Form[Computer],Seq[scala.Tuple2[String, String]]) => (Messages) => play.twirl.api.HtmlFormat.Appendable) = (computerForm,companies) => (messages) => apply(computerForm,companies)(messages)
+  def f:((Form[Computer],Seq[scala.Tuple2[String, String]]) => (RequestHeader,Messages) => play.twirl.api.HtmlFormat.Appendable) = (computerForm,companies) => (request,messages) => apply(computerForm,companies)(request,messages)
 
   def ref: this.type = this
 
@@ -75,11 +75,11 @@ Seq[Any](format.raw/*1.95*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Thu Jan 25 21:33:18 CST 2018
+                  DATE: Thu Jan 25 22:00:55 CST 2018
                   SOURCE: /home/decapo/apps/computer-database/app/views/createForm.scala.html
-                  HASH: b2569feedfe9c6840aa975fce15bf161ede2030e
-                  MATRIX: 785->1|951->97|988->115|1020->139|1099->94|1127->113|1155->188|1184->192|1195->196|1233->198|1269->208|1329->242|1369->273|1409->275|1454->293|1514->326|1594->385|1635->399|1723->466|1764->480|1856->551|1898->566|2130->777|2180->800|2367->960|2382->966|2422->985|2507->1040|2544->1047
-                  LINES: 21->1|24->3|26->5|26->5|27->1|29->4|30->5|32->7|32->7|32->7|34->9|36->11|36->11|36->11|38->13|40->15|40->15|41->16|41->16|42->17|42->17|44->19|49->24|52->27|56->31|56->31|56->31|59->34|61->36
+                  HASH: 26588486fabf2a83d981d30d08fba4c06176b731
+                  MATRIX: 799->1|989->121|1026->139|1058->163|1138->118|1166->137|1194->212|1223->216|1234->220|1272->222|1308->232|1368->266|1408->297|1448->299|1485->309|1500->315|1536->330|1573->340|1632->372|1712->431|1753->445|1841->512|1882->526|1974->597|2016->612|2248->823|2298->846|2485->1006|2500->1012|2540->1031|2625->1086|2662->1093
+                  LINES: 21->1|24->3|26->5|26->5|27->1|29->4|30->5|32->7|32->7|32->7|34->9|36->11|36->11|36->11|37->12|37->12|37->12|38->13|40->15|40->15|41->16|41->16|42->17|42->17|44->19|49->24|52->27|56->31|56->31|56->31|59->34|61->36
                   -- GENERATED --
               */
           
--- target/scala-2.12/twirl/main/views/html/editForm.template.scala
+++ target/scala-2.12/twirl/main/views/html/editForm.template.scala
@@ -15,16 +15,16 @@ import play.api.templates.PlayMagic._
 import play.api.mvc._
 import play.api.data._
 
-object editForm extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template4[Long,Form[Computer],Seq[scala.Tuple2[String, String]],Messages,play.twirl.api.HtmlFormat.Appendable] {
+object editForm extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template5[Long,Form[Computer],Seq[scala.Tuple2[String, String]],RequestHeader,Messages,play.twirl.api.HtmlFormat.Appendable] {
 
   /**/
-  def apply/*1.2*/(id: Long, computerForm: Form[Computer], companies : Seq[(String, String)])(implicit messages: Messages):play.twirl.api.HtmlFormat.Appendable = {
+  def apply/*1.2*/(id: Long, computerForm: Form[Computer], companies : Seq[(String, String)])(implicit request: RequestHeader, messages: Messages):play.twirl.api.HtmlFormat.Appendable = {
     _display_ {
       {
 /*3.2*/import helper._
 
 implicit def /*5.2*/implicitFieldConstructor/*5.26*/ = {{ FieldConstructor(twitterBootstrapInput.f) }};
-Seq[Any](format.raw/*1.106*/("""
+Seq[Any](format.raw/*1.130*/("""
 
 """),format.raw/*4.1*/("""
 """),format.raw/*5.75*/("""
@@ -34,9 +34,9 @@ Seq[Any](format.raw/*1.106*/("""
     """),format.raw/*9.5*/("""<h1>Edit computer</h1>
     
     """),_display_(/*11.6*/form(routes.Application.update(id))/*11.41*/ {_display_(Seq[Any](format.raw/*11.43*/("""
-        
+        """),_display_(/*12.10*/helper/*12.16*/.CSRF.formField),format.raw/*12.31*/("""
         """),format.raw/*13.9*/("""<fieldset>
-        
+        		
             """),_display_(/*15.14*/inputText(computerForm("name"), '_label -> "Computer name")),format.raw/*15.73*/("""
             """),_display_(/*16.14*/inputText(computerForm("introduced"), '_label -> "Introduced date")),format.raw/*16.81*/("""
             """),_display_(/*17.14*/inputText(computerForm("discontinued"), '_label -> "Discontinued date")),format.raw/*17.85*/("""
@@ -58,18 +58,19 @@ Seq[Any](format.raw/*1.106*/("""
     """)))}),format.raw/*33.6*/("""
     
     """),_display_(/*35.6*/form(routes.Application.delete(id), 'class -> "topRight")/*35.63*/ {_display_(Seq[Any](format.raw/*35.65*/("""
-        """),format.raw/*36.9*/("""<input type="submit" value="Delete this computer" class="btn danger">
-    """)))}),format.raw/*37.6*/("""
+				"""),_display_(/*36.6*/helper/*36.12*/.CSRF.formField),format.raw/*36.27*/("""
+        """),format.raw/*37.9*/("""<input type="submit" value="Delete this computer" class="btn danger">
+    """)))}),format.raw/*38.6*/("""
     
-""")))}),format.raw/*39.2*/("""
+""")))}),format.raw/*40.2*/("""
 """))
       }
     }
   }
 
-  def render(id:Long,computerForm:Form[Computer],companies:Seq[scala.Tuple2[String, String]],messages:Messages): play.twirl.api.HtmlFormat.Appendable = apply(id,computerForm,companies)(messages)
+  def render(id:Long,computerForm:Form[Computer],companies:Seq[scala.Tuple2[String, String]],request:RequestHeader,messages:Messages): play.twirl.api.HtmlFormat.Appendable = apply(id,computerForm,companies)(request,messages)
 
-  def f:((Long,Form[Computer],Seq[scala.Tuple2[String, String]]) => (Messages) => play.twirl.api.HtmlFormat.Appendable) = (id,computerForm,companies) => (messages) => apply(id,computerForm,companies)(messages)
+  def f:((Long,Form[Computer],Seq[scala.Tuple2[String, String]]) => (RequestHeader,Messages) => play.twirl.api.HtmlFormat.Appendable) = (id,computerForm,companies) => (request,messages) => apply(id,computerForm,companies)(request,messages)
 
   def ref: this.type = this
 
@@ -78,11 +79,11 @@ Seq[Any](format.raw/*1.106*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Thu Jan 25 21:33:18 CST 2018
+                  DATE: Thu Jan 25 22:04:56 CST 2018
                   SOURCE: /home/decapo/apps/computer-database/app/views/editForm.scala.html
-                  HASH: b5a6a6e5f479829e63619db8cf4e5473b7d07294
-                  MATRIX: 788->1|965->108|1002->126|1034->150|1114->105|1142->124|1170->199|1198->202|1209->206|1247->208|1283->218|1342->251|1386->286|1426->288|1471->306|1531->339|1611->398|1652->412|1740->479|1781->493|1873->564|1927->591|2159->802|2204->820|2389->978|2404->984|2444->1003|2529->1058|2566->1069|2632->1126|2672->1128|2708->1137|2813->1212|2850->1219
-                  LINES: 21->1|24->3|26->5|26->5|27->1|29->4|30->5|32->7|32->7|32->7|34->9|36->11|36->11|36->11|38->13|40->15|40->15|41->16|41->16|42->17|42->17|44->19|49->24|51->26|55->30|55->30|55->30|58->33|60->35|60->35|60->35|61->36|62->37|64->39
+                  HASH: de10271eeced15341958007e565e0ff462be79a4
+                  MATRIX: 802->1|1003->132|1040->150|1072->174|1152->129|1180->148|1208->223|1236->226|1247->230|1285->232|1321->242|1380->275|1424->310|1464->312|1501->322|1516->328|1552->343|1588->352|1650->387|1730->446|1771->460|1859->527|1900->541|1992->612|2046->639|2278->850|2323->868|2508->1026|2523->1032|2563->1051|2648->1106|2685->1117|2751->1174|2791->1176|2823->1182|2838->1188|2874->1203|2910->1212|3015->1287|3052->1294
+                  LINES: 21->1|24->3|26->5|26->5|27->1|29->4|30->5|32->7|32->7|32->7|34->9|36->11|36->11|36->11|37->12|37->12|37->12|38->13|40->15|40->15|41->16|41->16|42->17|42->17|44->19|49->24|51->26|55->30|55->30|55->30|58->33|60->35|60->35|60->35|61->36|61->36|61->36|62->37|63->38|65->40
                   -- GENERATED --
               */
           
